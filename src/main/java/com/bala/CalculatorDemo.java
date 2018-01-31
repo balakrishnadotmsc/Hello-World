591a517a9ba658d4e674502a9dcb8768adabe06c
Balakrishna Akkala <balakrishna.a@fluentgrid.com>
2018-01-31 18:31:29 +0530

    commit

---
+++ src/main/java/com/bala/CalculatorDemo.java
@@ -0,0 +1,11 @@
+package com.bala;
+
+public class CalculatorDemo {
+	public int addKey(int a,int b) {
+		return a+b;
+	}
+	
+	public int multKey(int a,int b) {
+		return a*b;
+	}
+}
--- target/Sample-0.0.1-SNAPSHOT/index.jsp
+++ target/Sample-0.0.1-SNAPSHOT/index.jsp
@@ -2,5 +2,6 @@
 <body>
 <h2>Hello World!</h2>
 sample application for jenkins
+welcome!master branch!!!
 </body>
 
